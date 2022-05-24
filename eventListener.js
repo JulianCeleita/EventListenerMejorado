d7b27cedbe9b02be3158f10d73f09c672ed19ff7
Julian Celeita <andresceleita@yahoo.es>
2022-05-23 23:18:18 -0500

    Añadimos el event listener modificado

--- eventListener.js
+++ eventListener.js
@@ -2,6 +2,14 @@
 const lista = document.getElementById('lista');
 const btnAgregar = document.getElementById('btn-agregar');
 
+// Si agrego un evento al elemento padre, lo agregará a todos los hijos aunque sean nuevos. Por eso trabajo la lista completa
+lista.addEventListener("click", (e) => {
+    if (e.target && e.target.tagName ==="A"){           // para que solo tome elementos tipo A y no los espacios entre ellos
+      e.target.classList.toggle("activo");
+    }
+});
+
+ 
 btnAgregar.addEventListener('click', () => {
 	const elemento = `
 		<a href="#">
